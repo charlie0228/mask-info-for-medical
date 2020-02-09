915e0060514333b1d5b02468871c8e3505e37684
AzureBlue <charlie800228@gmail.com>
2020-02-09 23:40:15 +0800

    fix: remove history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import Result from '../components/Result.vue';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.NODE_ENV === 'production'
     ? `/${process.env.VUE_APP_PUBLIC_PATH}/`
     : '/',
